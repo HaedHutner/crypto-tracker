a78e2a67d01d20f383eed2d39e4cfe1ffe5a8ddb
HaedHutner <mvvasilev@outlook.com>
2022-01-22 23:25:11 +0200

    Move average buy price calculator to own method

--- src/main/java/dev/haedhutner/controller/AverageBuyPriceController.java
+++ src/main/java/dev/haedhutner/controller/AverageBuyPriceController.java
@@ -7,6 +7,7 @@ import dev.haedhutner.binance.BinanceApi;
 import dev.haedhutner.binance.dto.AccountInfoDTO;
 import dev.haedhutner.controller.dto.AverageBuyPriceDTO;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 import io.javalin.plugin.openapi.dsl.OpenApiBuilder;
 import org.apache.commons.lang3.mutable.MutableDouble;
 import org.apache.commons.math3.stat.descriptive.WeightedEvaluation;
@@ -43,74 +44,76 @@ public class AverageBuyPriceController {
                 "/api/avgbuy",
                 OpenApiBuilder.documented(
                         OpenApiBuilder.document().jsonArray("200", AverageBuyPriceDTO.class),
-                        ctx -> {
-                            var result = new ArrayList<AverageBuyPriceDTO>();
-
-                            var portfolio = binanceApi.fetchPortfolio().balances.stream()
-                                    .filter(b -> (b.locked + b.free) > 0.0f)
-                                    .toList();
-
-                            portfolio.forEach(b -> {
-                                try {
-                                    logger.info("START " + b.asset);
-                                    var orders = binanceApi.fetchOrders(b.asset);
-
-                                    if (orders.size() < 1) {
-                                        return;
-                                    }
-
-                                    orders = orders.stream()
-                                            .filter(o -> Objects.equals(o.status, "FILLED") && Objects.equals(o.side, "BUY"))
-                                            .peek(o -> {
-                                                if (Objects.equals(o.type, "MARKET")) {
-                                                    try {
-                                                        o.price = binanceApi.fetchPriceAtTime(b.asset, o.time);
-                                                    } catch (IOException e) {
-                                                        e.printStackTrace();
-                                                    }
-                                                }
-                                            })
-                                            .sorted(Comparator.comparingLong(o -> o.time))
-                                            .collect(Collectors.collectingAndThen(
-                                                    Collectors.toList(),
-                                                    list -> {
-                                                        Collections.reverse(list);
-                                                        return list;
-                                                    })
-                                            );
-
-
-                                    var totalQty = b.free + b.locked;
-                                    MutableDouble accumQty = new MutableDouble(0.0d);
-
-                                    var firstElement = orders.get(0);
-
-                                    orders = orders.stream().takeWhile(o -> {
-                                        accumQty.add(o.executedQty);
-                                        return accumQty.doubleValue() <= totalQty + (totalQty * 0.01);
-                                    }).toList();
-
-                                    if (orders.size() < 1) {
-                                        orders = List.of(firstElement);
-                                    }
-
-                                    double[] values = orders.stream().mapToDouble(o -> o.price).toArray();
-                                    double[] weights = orders.stream().mapToDouble(o -> o.executedQty).toArray();
-
-                                    var dto = new AverageBuyPriceDTO();
-                                    dto.symbol = b.asset;
-                                    dto.avgBuyPrice = new Mean().evaluate(values, weights);
-
-                                    result.add(dto);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                                logger.info("END " + b.asset);
-                            });
-
-                            ctx.result(gson.toJson(result));
-                        }
+                        this::calculateAverageBuyPrice
                 )
         );
+    }
+
+    public void calculateAverageBuyPrice(Context ctx) {
+        var result = new ArrayList<AverageBuyPriceDTO>();
+
+        var portfolio = binanceApi.fetchPortfolio().balances.stream()
+                .filter(b -> (b.locked + b.free) > 0.0f)
+                .toList();
+
+        portfolio.forEach(b -> {
+            try {
+                logger.info("START " + b.asset);
+                var orders = binanceApi.fetchOrders(b.asset);
+
+                if (orders.size() < 1) {
+                    return;
+                }
+
+                orders = orders.stream()
+                        .filter(o -> Objects.equals(o.status, "FILLED") && Objects.equals(o.side, "BUY"))
+                        .peek(o -> {
+                            if (Objects.equals(o.type, "MARKET")) {
+                                try {
+                                    o.price = binanceApi.fetchPriceAtTime(b.asset, o.time);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        })
+                        .sorted(Comparator.comparingLong(o -> o.time))
+                        .collect(Collectors.collectingAndThen(
+                                Collectors.toList(),
+                                list -> {
+                                    Collections.reverse(list);
+                                    return list;
+                                })
+                        );
+
+
+                var totalQty = b.free + b.locked;
+                MutableDouble accumQty = new MutableDouble(0.0d);
+
+                var firstElement = orders.get(0);
+
+                orders = orders.stream().takeWhile(o -> {
+                    accumQty.add(o.executedQty);
+                    return accumQty.doubleValue() <= totalQty + (totalQty * 0.01);
+                }).toList();
+
+                if (orders.size() < 1) {
+                    orders = List.of(firstElement);
+                }
+
+                double[] values = orders.stream().mapToDouble(o -> o.price).toArray();
+                double[] weights = orders.stream().mapToDouble(o -> o.executedQty).toArray();
+
+                var dto = new AverageBuyPriceDTO();
+                dto.symbol = b.asset;
+                dto.avgBuyPrice = new Mean().evaluate(values, weights);
+
+                result.add(dto);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            logger.info("END " + b.asset);
+        });
+
+        ctx.result(gson.toJson(result));
     }
 }
